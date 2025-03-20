0b34d390b7925617a46a64aaf47154ca8331db3d
mmartinez1468 <165759311+mmartinez1468@users.noreply.github.com>
2025-03-20 22:29:22 +0100

    update

--- script.js
+++ script.js
@@ -11,4 +11,34 @@ const showMenu = (toggleId, navId) =>{
         toggle.classList.toggle('show-icon')
     })
   }
-  showMenu('nav-toggle','nav-menu');
+  showMenu('nav-toggle','nav-menu')
+
+
+
+  
+///////////////// REPLAYS ANIMATION WHEN BACK IN VIEWPORT /////////////////
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible'); // Remove to re-trigger animation
+      }
+    });
+  }, { threshold: 0.2 });
+  document.querySelectorAll('.animated').forEach(element => {
+    observer.observe(element);
+  });
+
+
+
+  ///////////////// PRE LOADER /////////////////
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader');
+    
+    loader.classList.add('loaderHidden');
+  
+    loader.addEventListener('transitioned', () => {
+      document.body.removeChild('loader');
+    })
+  })
